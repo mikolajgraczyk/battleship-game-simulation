ba219836f0973e28e649f67dae67b8bd43383eea
Mikołaj Graczyk <118211320+mikolajgraczyk@users.noreply.github.com>
2023-10-03 12:38:18 +0200

    created states for two players and created generateBoard function

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,41 @@
+import { useState } from "react";
 
+interface Setup {
+  rows: number;
+  cols: number;
+  ships: number[];
+}
+
+type Board = string[][];
+interface PlayerState {
+  board: Board;
+  destroyed: number;
+};
+
+const setup: Setup = {
+  rows: 10,
+  cols: 10,
+  ships: [5, 4, 3, 2, 1],
+};
 
 function App() {
-  return (
-    <></>
-  );
+  const generateBoard = (): Board => {
+    return Array(setup.rows)
+      .fill("empty")
+      .map(() => Array(setup.cols).fill("empty"));
+  };
+
+  const initializePlayerState = (): PlayerState => {
+    return {
+      board: generateBoard(),
+      destroyed: 0,
+    };
+  }
+
+  const [player1State, setPlayer1State] = useState<PlayerState>(initializePlayerState);
+  const [player2State, setPlayer2State] = useState<PlayerState>(initializePlayerState);
+
+  return <></>;
 }
 
 export default App;
